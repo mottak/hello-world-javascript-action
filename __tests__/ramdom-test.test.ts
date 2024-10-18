c89d3ead1276aba7bc9bea5ad233f970acff76c6
mottak <kissylamottapereira@gmail.com>
2024-10-18 11:21:43 -0300

    teste funcinando

--- __tests__/ramdom-test.test.ts
+++ __tests__/ramdom-test.test.ts
@@ -2,6 +2,6 @@ import { randomUUID } from 'node:crypto'
 
 describe('Suite', () => {
   it('should pass', () => {
-    expect(randomUUID()).toEqual(expect.any(Number))
+    expect(randomUUID()).toEqual(expect.any(String))
   })
 })
