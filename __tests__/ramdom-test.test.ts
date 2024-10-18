763416aed19805cd51801db6031e492329504755
mottak <kissylamottapereira@gmail.com>
2024-10-18 11:07:40 -0300

    teste falando

--- __tests__/ramdom-test.test.ts
+++ __tests__/ramdom-test.test.ts
@@ -2,6 +2,6 @@ import { randomUUID } from 'node:crypto'
 
 describe('Suite', () => {
   it('should pass', () => {
-    expect(randomUUID()).toEqual(expect.any(String))
+    expect(randomUUID()).toEqual(expect.any(Number))
   })
 })
